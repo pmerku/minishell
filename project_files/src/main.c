fd27604f557c18552fa535a2259b4fafbba01c55
Daniel Voort <techdanielvoort@gmail.com>
2020-03-01 18:02:37 +0100

    Tried making input fancy. It probably isn't any fancier now.

--- project_files/src/main.c
+++ project_files/src/main.c
@@ -15,6 +15,7 @@
 #include <memmgmt.h>
 #include <lex.h>
 #include <ft_env.h>
+#include <unistd.h>
 
 // literally just for env variable parsing testing
 // remove this once they work
@@ -41,6 +42,7 @@ int		main(int argc, char **argv, char **envp)
 	char			*line;
 	int 			gnl_ret;
 	t_linked_list	*lex_tokens;
+	char 			*working_dir;
 
 	env = env_from(envp);
 	env_print_all(env);
@@ -49,9 +51,9 @@ int		main(int argc, char **argv, char **envp)
 	gnl_ret = 1;
 	while (gnl_ret == 1)
 	{
-		ft_printf("&b&l$miniShell > &r");
+		working_dir = getcwd(NULL, 60);
+		ft_printf("\033[46;37m&f \xF0\x9F\x93\x81 %s&r ", working_dir);
 		gnl_ret = get_next_line(0, &line);
-		ft_printf("Execute: %s\n", line);
 		lex_tokens = lex(line);
 		debug_tokens(lex_tokens, env);
 		ft_free(line);
