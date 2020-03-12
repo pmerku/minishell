281cc3bdf3d9ff809af8f526a2fbfeee839c62ee
pmerku <48099552+pmerku@users.noreply.github.com>
2020-03-12 14:52:02 +0100

    Fix copying 1 byte too much

--- libft/src/ft_string/ft_memcpy.asm
+++ libft/src/ft_string/ft_memcpy.asm
@@ -22,10 +22,10 @@ FN_LABEL(ft_memcpy):
 	je		.return					;if equal return
 
 .loop:
+	cmp		rdx, 0x0				;check if 3rd parameter is 0
+    jz		.return					;if yes return
 	mov		al, byte [rsi + rcx]	;copy byte from src to a tmp location
 	mov		byte [rdi + rcx], al	;copy from tmp location to dst byte
-	cmp		rdx, 0x0				;check if 3rd parameter is 0
-	jz		.return					;if yes return
 
 	inc		rcx						;increment index of strings by 1
 	dec		rdx						;decrement 3rd parameter by 1
