9fa6b160519006921fe52d7c90817840b3b95109
Daniel Voort <techdanielvoort@gmail.com>
2020-03-26 11:45:20 +0100

    Fix segfault when empty string is provided, check GNL for errors

--- project_files/src/main.c
+++ project_files/src/main.c
@@ -19,6 +19,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <ft_parser.h>
+#include <ft_ctype.h>
+
+static int	is_all_space(char *str)
+{
+	while (*str != '\0' && ft_isspace(*str))
+		str++;
+	return (*str == '\0');
+}
 
 static void visualize_parse(t_parser_command ***commands, t_env *env)
 {
@@ -86,6 +94,17 @@ int		main(int argc, char **argv, char **envp)
 		working_dir = getcwd(NULL, 0);
 		ft_printf("\033[46;37m&f \xF0\x9F\x93\x81 %d %s&r ", 1, working_dir);
 		gnl_ret = get_next_line(0, &line);
+		if (gnl_ret < 0)
+		{
+			ft_printf("&cget_next_line error&r\n");
+			free(working_dir);
+		}
+		if (is_all_space(line))
+		{
+			ft_free(line);
+			free(working_dir);
+			continue ;
+		}
 		lex_tokens = lex(line, &err);
 		if (err != NULL)
 		{
