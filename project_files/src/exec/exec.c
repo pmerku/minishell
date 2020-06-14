e8d91a16b9b3e26abb9a9b05df4691364cc8fd74
Primož Merkù <48099552+pmerku@users.noreply.github.com>
2020-06-14 13:50:57 +0200

    Possible fix for pipes? Have to test on linux (maybe wsl is being weird)

--- project_files/src/exec/exec.c
+++ project_files/src/exec/exec.c
@@ -26,27 +26,16 @@
 #include <builtins.h>
 
 static t_builtin_list	g_builtin_list[] = {
-	{"echo", builtin_echo},
-	{"cd", builtin_cd},
-	{"pwd", builtin_pwd},
-	{"export", builtin_export},
-	{"unset", builtin_unset},
-	{"env", builtin_env},
-	{"exit", builtin_exit},
+		{"echo", builtin_echo},
+		{"cd", builtin_cd},
+		{"pwd", builtin_pwd},
+		{"export", builtin_export},
+		{"unset", builtin_unset},
+		{"env", builtin_env},
+		{"exit", builtin_exit},
 };
 
-//static int 		exec_num_simple_commands(t_parser_command **list)
-//{
-//	int 	i;
-//
-//	i = 0;
-//	while (list[i] != NULL) {
-//		i++;
-//	}
-//	return (i);
-//}
-
-static int 		exec_num_commands(t_parser_command ***commands)
+static int 				exec_num_commands(t_parser_command ***commands)
 {
 	int 	i;
 
@@ -57,7 +46,7 @@ static int 		exec_num_commands(t_parser_command ***commands)
 	return (i);
 }
 
-static char 				**parse_args(t_parser_command *command, t_env *env)
+static char 			**parse_args(t_parser_command *command, t_env *env)
 {
 	size_t		i;
 	size_t 		j;
@@ -80,56 +69,7 @@ static char 				**parse_args(t_parser_command *command, t_env *env)
 	return (arr);
 }
 
-//static t_simple_command 	*exec_launch(t_parser_command **list, t_env *env)
-//{
-////yeet	ft_printf("working on %p\n", list);
-//	t_parser_command	*command;
-//	t_redirection		**redirections;
-//	t_simple_command	*simple_command;
-//	int 				i;
-//
-//	command = *list;
-//	i = 0;
-//	while (command->arguments[i] != NULL) {
-//		i++;
-//	}
-//	simple_command = ft_checked_calloc(1, sizeof(t_simple_command));
-//	if (!simple_command) {
-//		return (NULL);
-//	}
-//	simple_command->args = (char **)ft_checked_calloc(i + 1, sizeof(char *));
-//	if (!simple_command->args) {
-//		return (NULL);
-//	}
-//	i = 0;
-//	while (command->arguments[i] != NULL) {
-//		simple_command->args[i] = env_parse_string(env, command->arguments[i]);
-//		//yeet	ft_printf("COMMAND ARGS ==> [%s]\n", simple_command->args[i]);
-//		i++;
-//	}
-//	i = 0;
-//	redirections = command->redirections_in;
-//	while (redirections != NULL && redirections[i] != NULL) {
-//		i++;
-//	}
-//	if (redirections != NULL && redirections[i - 1] != NULL) {
-//		simple_command->infile = env_parse_string(env, redirections[i - 1]->file);
-//		//yeet	ft_printf("INFILE ==> [%s]\n", simple_command->infile);
-//	}
-//	i = 0;
-//	redirections = command->redirections_out;
-//	while (redirections != NULL && redirections[i] != NULL) {
-//		i++;
-//	}
-//	if (redirections != NULL && redirections[i - 1] != NULL) {
-//		simple_command->outfile = env_parse_string(env, redirections[i - 1]->file);
-//		simple_command->type= redirections[i - 1]->type;
-//		//yeet	ft_printf("OUTFILE ==> [%s]\n", simple_command->outfile);
-//	}
-//	return (simple_command);
-//}
-
-static int 		get_input(t_parser_command *command, t_executor *exec, t_env *env, int prev_fd)
+static int 				get_input(t_parser_command *command, t_executor *exec, t_env *env, int prev_fd)
 {
 	size_t			i;
 	t_redirection	*redir;
@@ -164,7 +104,7 @@ static int 		get_input(t_parser_command *command, t_executor *exec, t_env *env, 
 	return (0);
 }
 
-static int 		get_output(t_parser_command *command, t_executor *exec, t_env *env, int last_command)
+static int 				get_output(t_parser_command *command, t_executor *exec, t_env *env, int last_command)
 {
 	size_t			i;
 	t_redirection	*redir;
@@ -173,7 +113,8 @@ static int 		get_output(t_parser_command *command, t_executor *exec, t_env *env,
 	i = 0;
 	if (command->redirections_out == NULL)
 	{
-		pipe(exec->fd_pipe); // TODO Protect this
+		if (pipe(exec->fd_pipe) == -1)
+			return(1);
 		exec->fd_out = exec->fd_pipe[1];
 		exec->fd_in = exec->fd_pipe[0];
 		if (last_command)
@@ -206,7 +147,7 @@ static int 		get_output(t_parser_command *command, t_executor *exec, t_env *env,
 	return (0);
 }
 
-static int		exec_command2(t_parser_command **list, t_env *env)
+static int				exec_command2(t_parser_command **list, t_env *env)
 {
 	t_executor			exec;
 	t_parser_command 	*command;
@@ -221,11 +162,12 @@ static int		exec_command2(t_parser_command **list, t_env *env)
 
 	exec.tmp_in = dup(0);
 	exec.tmp_out = dup(1);
-
 	while (list[i] != NULL)
 	{
 		command = list[i];
 		args = parse_args(command, env);
+		dup2(exec.fd_in, 0);
+		close(exec.fd_in);
 		if (args == NULL)
 		{
 			ft_printf("&cFailed to parse args with env vars\n&r");
@@ -236,19 +178,20 @@ static int		exec_command2(t_parser_command **list, t_env *env)
 		if (err != 0)
 		{
 			// TODO Clean up any opened pipes / unfreed memory
+			ft_printf("ERROR INPUT\n");
 			return (err);
 		}
 		err = get_output(command, &exec, env, list[i + 1] == NULL);
 		if (err != 0)
 		{
 			// TODO Clean up any opened pipes / unfreed memory
+			ft_printf("ERROR OUTPUT\n");
 			return (err);
 		}
 		dup2(exec.fd_out, 1);
 		close(exec.fd_out);
-		dup2(exec.fd_in, 0);
-		close(exec.fd_in);
-		// TODO Handle logic
+//		dup2(exec.fd_in, 0);
+//		close(exec.fd_in);
 		size_t j = 0;
 		int builtin = 0;
 		while (j < sizeof(g_builtin_list) / sizeof(g_builtin_list[0])) {
@@ -294,110 +237,7 @@ static int		exec_command2(t_parser_command **list, t_env *env)
 	return (WEXITSTATUS(exec.status));
 }
 
-//static int		exec_command(t_parser_command **list, t_env *env)
-//{
-//	t_executor			exec;
-//	t_simple_command	*simple_command;
-//	int		i;
-//	int		list_size;
-//
-//	exec.tmp_in = dup(0);
-//	exec.tmp_out = dup(1);
-//
-//	simple_command = exec_launch(list, env);
-//	if (!simple_command) {
-//		return (0);
-//	}
-//
-//	if (simple_command->infile != NULL) {
-//		exec.fd_in = open(simple_command->infile, O_RDONLY);
-//		if (exec.fd_in == -1) {
-//			ft_printf("&cError opening file &f%s&c: &f%s&r\n", simple_command->infile, strerror(errno));
-//			return (1);
-//			// TODO Clean up properly
-//		}
-//	}
-//	else {
-//		exec.fd_in = dup(exec.tmp_in);
-//	}
-//	i = 0;
-//	list_size = exec_num_simple_commands(list);
-//	//yeet	ft_printf("NUM OF PIPES: [%d]\n", list_size - 1);
-//	while (i < list_size) {
-//		//yeet	ft_printf("PIPE INDEX: [%d]\n", i);
-//		if (simple_command == NULL) {
-//			list++;
-//			simple_command = exec_launch(list, env);
-//		}
-//		dup2(exec.fd_in, 0);
-//		close(exec.fd_in);
-//		if (i == list_size - 1) {
-//			if (simple_command->outfile != NULL) {
-//				exec.fd_out = open(simple_command->outfile, O_WRONLY | O_CREAT | (simple_command->type == APPEND ? O_APPEND : O_TRUNC), 0664);
-//				if (exec.fd_out == -1) {
-//					ft_printf("&cError opening file &f%s&c: &f%s&r\n", simple_command->outfile, strerror(errno));
-//					// TODO Close shit properly
-//					return (1);
-//				}
-//			}
-//			else {
-//				exec.fd_out = dup(exec.tmp_out);
-//			}
-//		}
-//		else {
-//			pipe(exec.fd_pipe);
-//			exec.fd_out = exec.fd_pipe[1];
-//			exec.fd_in = exec.fd_pipe[0];
-//		}
-//		dup2(exec.fd_out, 1);
-//		close(exec.fd_out);
-//		size_t j = 0;
-//		int builtin = 0;
-//		while (j < sizeof(g_builtin_list) / sizeof(g_builtin_list[0])) {
-//			if (ft_strcmp(simple_command->args[0], g_builtin_list[j].name) == 0) {
-//				g_builtin_list[j].func(simple_command->args, env);
-//				builtin = 1;
-//				break ;
-//			}
-//			j++;
-//		}
-//		if (builtin == 0)
-//		{
-//			exec.pid = fork();
-//			if (exec.pid == 0)
-//			{
-//				char *path = env_resolve_path_file(env, simple_command->args[0]);
-//				if (path == NULL)
-//				{
-//					ft_printf("&cCommand not found: &f%s\n", simple_command->args[0]);
-//					exit(EXIT_FAILURE);
-//				}
-//				execve(path, simple_command->args, env->vars);
-//				ft_printf("Something went wrong! (execve) %s\n", strerror(errno));
-//				exit(EXIT_FAILURE);
-//			}
-//			else if (exec.pid < 0)
-//			{
-//				ft_printf("Something went wrong! (fork)\n");
-//				exit(EXIT_FAILURE);
-//			}
-//		}
-//		ft_free(simple_command);
-//		simple_command = NULL;
-//		i++;
-//	}
-//
-//	dup2(exec.tmp_in, 0);
-//	dup2(exec.tmp_out, 1);
-//	close(exec.tmp_in);
-//	close(exec.tmp_out);
-//
-//	exec.status = 0;
-//	waitpid(exec.pid, &exec.status, WUNTRACED);
-//	return (WEXITSTATUS(exec.status));
-//}
-
-int				execute(t_parser_command ***commands, t_env *env)
+int						execute(t_parser_command ***commands, t_env *env)
 {
 	int 				i;
 	int 				ret;
