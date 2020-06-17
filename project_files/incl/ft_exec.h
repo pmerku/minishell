1bc038d5ca77f559ae2bf1afd222a6e8e94a5bf9
Primož Merkù <48099552+pmerku@users.noreply.github.com>
2020-06-17 20:52:09 +0200

    Exit status added

--- project_files/incl/ft_exec.h
+++ project_files/incl/ft_exec.h
@@ -13,15 +13,18 @@
 #ifndef FT_EXEC_H
 # define FT_EXEC_H
 
-#include <ft_env.h>
-#include <ft_parser.h>
-#include <ft_lex.h>
-#include <sys/types.h>
+# include <ft_env.h>
+# include <ft_parser.h>
+# include <ft_lex.h>
+# include <sys/types.h>
 
 # define STANDARD_IN	0
 # define STANDARD_OUT	1
 # define PIPE_IN		0
 # define PIPE_OUT		1
+
+# define COMMAND_NOT_FOUND	127
+# define SIGNAL_TERMINATED	128
 
 typedef struct	s_executor {
 	int 	tmp_in;
